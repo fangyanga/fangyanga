ede3ed1f7267ac6f877ee1f64dfa0be99d51391d
Alfredo Linguini <a.linguini@ratatouille.fr>
2021-11-24 20:43:15 +0000

    update

--- munge/01-A.R
+++ munge/01-A.R
@@ -1,1 +1,10 @@
 # Example preprocessing script.
+gender <- cyber.security.1_enrolments %>%
+  select(learner_id, gender)
+
+
+
+#Combine the enrolments and remove the same student ids to find the total number of students
+library(dplyr)
+cyber.security.enrolments_1=bind_rows(cyber.security.1_enrolments,cyber.security.2_enrolments,cyber.security.3_enrolments,cyber.security.4_enrolments,cyber.security.5_enrolments,cyber.security.6_enrolments,cyber.security.7_enrolments)
+cyber.security.enrolments=cyber.security.enrolments_1[!duplicated(cyber.security.enrolments_1$learner_id), ]
--- src/eda.R
+++ src/eda.R
@@ -1,8 +1,20 @@
+setwd("/Users/wangyahan/Desktop/Data Management and Explor/DM")
 library('ProjectTemplate')
 load.project()
+#Draw the age range distribution
+age_range = cyber.security.enrolments %>% filter(age_range != "Unknown") 
 
-for (dataset in project.info$data)
-{
-  message(paste('Showing top 5 rows of', dataset))
-  print(head(get(dataset)))
-}
+ggplot(age_range,aes(x=age_range, fill=age_range))+geom_bar() 
+
+highest_education_level = cyber.security.enrolments %>% filter(highest_education_level  != "Unknown") 
+
+ggplot(highest_education_level,aes(x=highest_education_level, fill=highest_education_level))+geom_bar()  
+
+employment_status = cyber.security.enrolments %>% filter(employment_status  != "Unknown") 
+
+ggplot(employment_status,aes(x=employment_status, fill=employment_status))+geom_bar()  
+
+employment_area = cyber.security.enrolments %>% filter(employment_area  != "Unknown")
+
+ggplot(employment_area,aes(x=employment_area, fill=employment_area))+geom_bar()
+
